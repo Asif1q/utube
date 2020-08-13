f3f7710ba11712317f7c24e0a7025f7fb19abe66
Asif1q <54772037+Asif1q@users.noreply.github.com>
2020-08-13 09:19:22 +0530

    DOWNLOAD_LOCATION = Config.DOWNLOAD_LOCATION

--- bot/config.py
+++ bot/config.py
@@ -9,6 +9,9 @@ class Config:
     API_ID = int(os.environ.get("API_ID"))
 
     API_HASH = os.environ.get("API_HASH")
+    
+    DOWNLOAD_LOCATION = os.environ.get("DOWNLOAD_LOCATION")
+
 
     CLIENT_ID = os.environ.get("CLIENT_ID")
 
